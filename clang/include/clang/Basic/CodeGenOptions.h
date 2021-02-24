d345dbf4d08a3bcc1ff8bb67a855792969c675ca
Bryan Chan <bryan.chan@huawei.com>
2021-02-24 18:50:30 -0500

    Fix -fveclib=PGMATH
    The use of #ifdef in include/clang/Driver/Options.td was incorrect and
    unsupported. As a result -fveclib=PGMATH was silently ignored, and
    in LLVM 12, it causes the invocation to fail. This patch unguards the
    option so that it is parsed correctly, but lets the FLANG_LLVM_EXTENSIONS
    macro continue to toggle the feature.

--- clang/include/clang/Basic/CodeGenOptions.h
+++ clang/include/clang/Basic/CodeGenOptions.h
@@ -59,9 +59,7 @@ public:
     Accelerate, // Use the Accelerate framework.
     LIBMVEC,    // GLIBC vector math library.
     MASSV,      // IBM MASS vector library.
-#ifdef FLANG_LLVM_EXTENSIONS
     PGMATH,     // PGI math library.
-#endif
     SVML,              // Intel short vector math library.
     Darwin_libsystem_m // Use Darwin's libsytem_m vector functions.
   };
