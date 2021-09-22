d24d8609cb47f5340dc1d4f59b0ed5a2d8c5a48d
Itay Bookstein <itay.bookstein@nextsilicon.com>
2021-09-22 17:12:19 +0300

    [ClassicFlang][Driver] Fix warnings
    * -Wsuggest-override for ClassicFlangMacroBuilder::defineMacro
    * -Wrange-loop-construct when iterating over llvm::opt::Arg::getValues
    
    Signed-off-by: Itay Bookstein <itay.bookstein@nextsilicon.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5436,7 +5436,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     PassRemarkVal = ".*";
     Args.ClaimAllArgs(options::OPT_Minfoall);
   } else if (Arg *A = Args.getLastArg(options::OPT_Minfo_EQ)) {
-    for (const StringRef &val : A->getValues()) {
+    for (StringRef val : A->getValues()) {
       if (val.equals("all")) {
         PassRemarkVal = ".*";
         break;
@@ -5462,7 +5462,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     PassRemarkVal = ".*";
     Args.ClaimAllArgs(options::OPT_Mneginfoall);
   } else if (Arg *A = Args.getLastArg(options::OPT_Mneginfo_EQ)) {
-    for (const StringRef &val : A->getValues()) {
+    for (StringRef val : A->getValues()) {
       if (val.equals("all")) {
         PassRemarkVal = ".*";
         break;
