da1e3fdcd1e5e0378f538471458a4f6d3d87d655
Richard Barton <richard.barton@arm.com>
2021-11-12 17:10:05 +0000

    [driver] Pass LLVM target_features to flang
    Pass LLVM target features (-mattr strings) to flang to embed in generated
    .ll files. For normal compilation this won't make much difference as the
    attributes are passed to clang after flang2 and can be applied there but this
    is crucial to enable LTO with flang as clang will not apply the attributes
    when building the flang2 output. libLTO will need to read these out of the
    object files to apply them.
    
    Signed-off-by: Richard Barton <richard.barton@arm.com>

---
+++ clang/test/Driver/emit-flang-attrs.f90
@@ -0,0 +1,10 @@
+// REQUIRES: aarch64-registered-target
+// RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-NEON %s
+// RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a+sve -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-SVE %s
+// RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a+nosve -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-NOSVE %s
+// CHECK-ATTRS-NEON: "{{.*}}flang2"
+// CHECK-ATTRS-NEON-SAME: "-target_features" "+neon"
+// CHECK-ATTRS-SVE: "{{.*}}flang2"
+// CHECK-ATTRS-SVE-SAME: "-target_features" "+neon,+sve"
+// CHECK-ATTRS-NOSVE: "{{.*}}flang2"
+// CHECK-ATTRS-NOSVE-SAME: "-target_features" "+neon,-sve"
