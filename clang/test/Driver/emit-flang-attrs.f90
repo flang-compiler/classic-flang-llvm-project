613d84f40943eea4fb8b76338f3fa7dc416dd811
Richard Barton <richard.barton@arm.com>
2021-11-18 11:16:20 +0000

    [flang-driver] Uniquify the target_features string
    Use existing clang support for uniquifying target feature lists to pass
    a unique list through to flang.

--- clang/test/Driver/emit-flang-attrs.f90
+++ clang/test/Driver/emit-flang-attrs.f90
@@ -2,6 +2,7 @@
 // RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-NEON %s
 // RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a+sve -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-SVE %s
 // RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a+nosve -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-NOSVE %s
+// RUN: %flang -### -target aarch64-linux-gnu -march=armv8-a+sve+nosve -c %s 2>&1 | FileCheck --check-prefix=CHECK-ATTRS-NOSVE %s
 // CHECK-ATTRS-NEON: "{{.*}}flang2"
 // CHECK-ATTRS-NEON-SAME: "-target_features" "+neon"
 // CHECK-ATTRS-SVE: "{{.*}}flang2"
