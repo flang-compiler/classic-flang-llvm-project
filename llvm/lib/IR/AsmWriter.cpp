ff6e6e6a8bed0b09830fe44e55d7e884f8bf41ee
Alok Kumar Sharma <AlokKumar.Sharma@amd.com>
2022-03-09 07:50:02 +0530

    [DebugInfo] Enhance DIImportedEntity to accept children entities (#83)
    New field `elements` is added to '!DIImportedEntity', representing
    list of aliased entities.
    This is needed to dump optimized debugging information where all names
    in a module are imported, but a few names are imported with overriding
    aliases.
    
    Reviewed By: dblaikie
    
    Differential Revision: https://reviews.llvm.org/D109343

--- llvm/lib/IR/AsmWriter.cpp
+++ llvm/lib/IR/AsmWriter.cpp
@@ -2394,6 +2394,7 @@ static void writeDIImportedEntity(raw_ostream &Out, const DIImportedEntity *N,
   Printer.printMetadata("entity", N->getRawEntity());
   Printer.printMetadata("file", N->getRawFile());
   Printer.printInt("line", N->getLine());
+  Printer.printMetadata("elements", N->getRawElements());
   Out << ")";
 }
 
