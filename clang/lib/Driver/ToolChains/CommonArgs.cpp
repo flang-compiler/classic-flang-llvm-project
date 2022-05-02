c742b6fca4e18e50b0f3100050a417b91302895a
Bryan Chan <bryan.chan@huawei.com>
2022-05-01 21:43:45 -0400

    [ClassicFlang][Driver] Coexist with LLVM Flang
    Make TableGen respect the ENABLE_CLASSIC_FLANG macro and better separate the
    driver options that are mutually exclusive. When the macro is defined, do not
    build LLVM Flang at all.

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -342,6 +342,7 @@ static bool shouldIgnoreUnsupportedTargetFeature(const Arg &TargetFeatureArg,
   return TargetFeatureArg.getOption().matches(options::OPT_mno_cumode);
 }
 
+#ifdef ENABLE_CLASSIC_FLANG
 /// \brief Determine if Fortran "main" object is needed
 static bool needFortranMain(const Driver &D, const ArgList &Args) {
   return (needFortranLibs(D, Args)
@@ -358,6 +359,7 @@ bool tools::needFortranLibs(const Driver &D, const ArgList &Args) {
 
   return false;
 }
+#endif
 
 void tools::addPathIfExists(const Driver &D, const Twine &Path,
                             ToolChain::path_list &Paths) {
@@ -467,7 +469,9 @@ void tools::AddLinkerInputs(const ToolChain &TC, const InputInfoList &Inputs,
                             const ArgList &Args, ArgStringList &CmdArgs,
                             const JobAction &JA) {
   const Driver &D = TC.getDriver();
+#ifdef ENABLE_CLASSIC_FLANG
   bool SeenFirstLinkerInput = false;
+#endif
 
   // Add extra linker input arguments which are not treated as inputs
   // (constructed via -Xarch_).
@@ -501,6 +505,7 @@ void tools::AddLinkerInputs(const ToolChain &TC, const InputInfoList &Inputs,
     if (II.isNothing())
       continue;
 
+#ifdef ENABLE_CLASSIC_FLANG
     // Add Fortan "main" before the first linker input
     if (!SeenFirstLinkerInput) {
       if (needFortranMain(D, Args)) {
@@ -508,7 +513,7 @@ void tools::AddLinkerInputs(const ToolChain &TC, const InputInfoList &Inputs,
       }
       SeenFirstLinkerInput = true;
     }
-
+#endif
     // Otherwise, this is a linker input argument.
     const Arg &A = II.getInputArg();
 
@@ -553,7 +558,7 @@ void tools::AddLinkerInputs(const ToolChain &TC, const InputInfoList &Inputs,
       addArchSpecificRPath(TC, Args, CmdArgs);
     }
   }
-
+#ifdef ENABLE_CLASSIC_FLANG
   if (!SeenFirstLinkerInput && needFortranMain(D, Args)) {
     CmdArgs.push_back("-lflangmain");
   }
@@ -562,6 +567,7 @@ void tools::AddLinkerInputs(const ToolChain &TC, const InputInfoList &Inputs,
   for (auto Arg : Args.filtered(options::OPT_no_fortran_main, options::OPT_Mnomain)) {
     Arg->claim();
   }
+#endif
 }
 
 void tools::addLinkerCompressDebugSectionsOption(
--- clang/lib/Driver/ToolChains/Linux.cpp
+++ clang/lib/Driver/ToolChains/Linux.cpp
@@ -632,6 +632,7 @@ std::string Linux::getDynamicLinker(const ArgList &Args) const {
   return "/" + LibDir + "/" + Loader;
 }
 
+#ifdef ENABLE_CLASSIC_FLANG
 /// Convert path list to Fortran frontend argument
 static void AddFlangSysIncludeArg(const ArgList &DriverArgs,
                                   ArgStringList &Flang1Args,
@@ -819,6 +820,7 @@ void Linux::AddFlangSystemIncludeArgs(const ArgList &DriverArgs,
 
   AddFlangSysIncludeArg(DriverArgs, Flang1Args, IncludePathList);
 }
+#endif
 
 void Linux::AddClangSystemIncludeArgs(const ArgList &DriverArgs,
                                       ArgStringList &CC1Args) const {
