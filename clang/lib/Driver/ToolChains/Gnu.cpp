1f77ebcdc5c721f8b6ca83329f550ed710928fce
Bryan Chan <bryan.chan@huawei.com>
2022-05-01 21:43:45 -0400

    [ClassicFlang][Driver] Coexist with LLVM Flang
    Make TableGen respect the ENABLE_CLASSIC_FLANG macro and better separate the
    driver options that are mutually exclusive. When the macro is defined, do not
    build LLVM Flang at all.

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -548,6 +548,7 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   // The profile runtime also needs access to system libraries.
   getToolChain().addProfileRTLibs(Args, CmdArgs);
 
+#ifdef ENABLE_CLASSIC_FLANG
   // Add Fortran runtime libraries
   if (needFortranLibs(D, Args)) {
     ToolChain.AddFortranStdlibLibArgs(Args, CmdArgs);
@@ -557,6 +558,7 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
       Arg->claim();
     }
   }
+#endif
 
   if (D.CCCIsCXX() &&
       !Args.hasArg(options::OPT_nostdlib, options::OPT_nodefaultlibs,
