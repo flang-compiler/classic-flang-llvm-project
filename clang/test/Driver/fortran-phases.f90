4e823b32d0d0a05ad09ac4fec7ea317a4a2340aa
Richard Barton <richard.barton@arm.com>
2022-05-03 17:38:49 +0100

    Fix flang driver preprocessor issue
    Fix a bug where flang -E -save-temps input.F90 would preprocess
    the input file twice. Add a new test for preprocessor behaviour that
    would expose the bug and updated fortran-phases test for new behaviour.
    
    Also added -emit-flang-llvm to that test for completeness.
    
    Signed-off-by: Richard Barton <richard.barton@arm.com>

--- clang/test/Driver/fortran-phases.f90
+++ clang/test/Driver/fortran-phases.f90
@@ -7,6 +7,7 @@
 ! RUN: %flang -ccc-print-phases -S 2>&1 %s | FileCheck %s --check-prefix=AONLY-NOPP
 ! RUN: %flang -ccc-print-phases -c -emit-llvm 2>&1 %s | FileCheck %s --check-prefix=LLONLY-NOPP
 ! RUN: %flang -ccc-print-phases -S -emit-llvm 2>&1 %s | FileCheck %s --check-prefix=LLONLY-NOPP
+! RUN: %flang -ccc-print-phases -emit-flang-llvm 2>&1 %s | FileCheck %s --check-prefix=FLLONLY-NOPP
 ! RUN: %flang -ccc-print-phases -fsyntax-only 2>&1 %s | FileCheck %s --check-prefix=SONLY-NOPP
 ! RUN: %flang -ccc-print-phases -E 2>&1 %s | FileCheck %s --check-prefix=PPONLY-NOPP
 
@@ -16,6 +17,7 @@
 ! RUN: %flang -ccc-print-phases -S 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=AONLY
 ! RUN: %flang -ccc-print-phases -c -emit-llvm 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=LLONLY
 ! RUN: %flang -ccc-print-phases -S -emit-llvm 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=LLONLY
+! RUN: %flang -ccc-print-phases -emit-flang-llvm 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=FLLONLY
 ! RUN: %flang -ccc-print-phases -fsyntax-only 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=SONLY
 ! RUN: %flang -ccc-print-phases -E 2>&1 -x f95-cpp-input %s | FileCheck %s --check-prefix=PPONLY
 
@@ -29,31 +31,39 @@
 ! CONLY-NOPP: 1: compiler, {0}, ir
 ! CONLY-NOPP: 2: backend, {1}, assembler
 ! CONLY-NOPP: 3: assembler, {2}, object
-! CONLY-NOPP-NOT: 4: linker, {3}, image
+! CONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! AONLY-NOPP: 0: input, {{.*}}, f95
 ! AONLY-NOPP: 1: compiler, {0}, ir
 ! AONLY-NOPP: 2: backend, {1}, assembler
-! AONLY-NOPP-NOT: 3: assembler, {2}, object
-! AONLY-NOPP-NOT: 4: linker, {3}, image
+! AONLY-NOPP-NOT: {{.*}}: assembler, {{{.*}}}, object
+! AONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! LLONLY-NOPP: 0: input, {{.*}}, f95
 ! LLONLY-NOPP: 1: compiler, {0}, ir
-! LLONLY-NOPP-NOT: 2: backend, {1}, assembler
-! LLONLY-NOPP-NOT: 3: assembler, {2}, object
-! LLONLY-NOPP-NOT: 4: linker, {3}, image
+! LLONLY-NOPP-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! LLONLY-NOPP-NOT: {{.*}}: assembler, {{{.*}}}, object
+! LLONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
+
+! FLLONLY-NOPP: 0: input, {{.*}}, f95
+! FLLONLY-NOPP: 1: compiler, {0}, ir
+! FLLONLY-NOPP-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! FLLONLY-NOPP-NOT: {{.*}}: assembler, {{{.*}}}, object
+! FLLONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! SONLY-NOPP: 0: input, {{.*}}, f95
-! SONLY-NOPP-NOT: 1: compiler, {0}, ir
-! SONLY-NOPP-NOT: 2: backend, {1}, assembler
-! SONLY-NOPP-NOT: 3: assembler, {2}, object
-! SONLY-NOPP-NOT: 4: linker, {3}, image
+! SONLY-NOPP-NOT: {{.*}}: compiler, {{{.*}}}, ir
+! SONLY-NOPP-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! SONLY-NOPP-NOT: {{.*}}: assembler, {{{.*}}}, object
+! SONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
 
+! flang always preprocesses with -E regardless of file extension
 ! PPONLY-NOPP: 0: input, {{.*}}, f95
-! PPONLY-NOPP: 1: compiler, {0}, ir
-! PPONLY-NOPP-NOT: 2: backend, {1}, assembler
-! PPONLY-NOPP-NOT: 3: assembler, {2}, object
-! PPONLY-NOPP-NOT: 4: linker, {3}, image
+! PPONLY-NOPP: 1: preprocessor, {0}, f95
+! PPONLY-NOPP-NOT: {{.*}}: compiler, {{{.*}}}, ir
+! PPONLY-NOPP-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! PPONLY-NOPP-NOT: {{.*}}: assembler, {{{.*}}}, object
+! PPONLY-NOPP-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! LINK: 0: input, {{.*}}, f95-cpp-input
 ! LINK: 1: preprocessor, {0}, f95
@@ -67,35 +77,42 @@
 ! CONLY: 2: compiler, {1}, ir
 ! CONLY: 3: backend, {2}, assembler
 ! CONLY: 4: assembler, {3}, object
-! CONLY-NOT: 5: linker, {4}, image
+! CONLY-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! AONLY: 0: input, {{.*}}, f95-cpp-input
 ! AONLY: 1: preprocessor, {0}, f95
 ! AONLY: 2: compiler, {1}, ir
 ! AONLY: 3: backend, {2}, assembler
-! AONLY-NOT: 4: assembler, {3}, object
-! AONLY-NOT: 5: linker, {4}, image
+! AONLY-NOT: {{.*}}: assembler, {{{.*}}}, object
+! AONLY-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! LLONLY: 0: input, {{.*}}, f95-cpp-input
 ! LLONLY: 1: preprocessor, {0}, f95
 ! LLONLY: 2: compiler, {1}, ir
-! LLONLY-NOT: 3: backend, {2}, assembler
-! LLONLY-NOT: 4: assembler, {3}, object
-! LLONLY-NOT: 5: linker, {4}, image
+! LLONLY-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! LLONLY-NOT: {{.*}}: assembler, {{{.*}}}, object
+! LLONLY-NOT: {{.*}}: linker, {{{.*}}}, image
+
+! FLLONLY: 0: input, {{.*}}, f95-cpp-input
+! FLLONLY: 1: preprocessor, {0}, f95
+! FLLONLY: 2: compiler, {1}, ir
+! FLLONLY-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! FLLONLY-NOT: {{.*}}: assembler, {{{.*}}}, object
+! FLLONLY-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! SONLY: 0: input, {{.*}}, f95-cpp-input
 ! SONLY: 1: preprocessor, {0}, f95
-! SONLY-NOT: 2: compiler, {1}, ir
-! SONLY-NOT: 3: backend, {2}, assembler
-! SONLY-NOT: 4: assembler, {3}, object
-! SONLY-NOT: 5: linker, {4}, image
+! SONLY-NOT: {{.*}}: compiler, {{{.*}}}, ir
+! SONLY-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! SONLY-NOT: {{.*}}: assembler, {{{.*}}}, object
+! SONLY-NOT: {{.*}}: linker, {{{.*}}}, image
 
 ! PPONLY: 0: input, {{.*}}, f95-cpp-input
 ! PPONLY: 1: preprocessor, {0}, f95
-! PPONLY: 2: compiler, {1}, ir
-! PPONLY-NOT: 3: backend, {2}, assembler
-! PPONLY-NOT: 4: assembler, {3}, object
-! PPONLY-NOT: 5: linker, {4}, image
+! PPONLY-NOT: {{.*}}: compiler, {{{.*}}}, ir
+! PPONLY-NOT: {{.*}}: backend, {{{.*}}}, assembler
+! PPONLY-NOT: {{.*}}: assembler, {{{.*}}}, object
+! PPONLY-NOT: {{.*}}: linker, {{{.*}}}, image
 
 program hello
   write(*, *) "Hello"
