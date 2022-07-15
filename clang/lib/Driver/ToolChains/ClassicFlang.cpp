4fa1650f67c4a90d817d8e4645fc58169b2f7324
Paul Osmialowski <pawel.osmialowski@arm.com>
2022-07-15 09:17:08 +0100

    classic flang: enable IEEE by default (and sort Lower/Common inconsistency out)
    Signed-off-by: Paul Osmialowski <pawel.osmialowski@arm.com>

--- clang/lib/Driver/ToolChains/ClassicFlang.cpp
+++ clang/lib/Driver/ToolChains/ClassicFlang.cpp
@@ -64,7 +64,7 @@ void ClassicFlang::ConstructJob(Compilation &C, const JobAction &JA,
   ArgStringList LowerCmdArgs;
   SmallString<256> Stem;
   std::string OutFile;
-  bool NeedIEEE = false;
+  bool NeedIEEE = true;
   bool NeedFastMath = false;
   bool NeedRelaxedMath = false;
 
@@ -594,7 +594,7 @@ void ClassicFlang::ConstructJob(Compilation &C, const JobAction &JA,
     LowerCmdArgs.push_back("-x");
     LowerCmdArgs.push_back("216");
     LowerCmdArgs.push_back("1");
-    // Lower: -ieee 0
+    // Common: -ieee 0
     CommonCmdArgs.push_back("-ieee");
     CommonCmdArgs.push_back("0");
   } else if (NeedIEEE) {
@@ -622,7 +622,7 @@ void ClassicFlang::ConstructJob(Compilation &C, const JobAction &JA,
     LowerCmdArgs.push_back("-y");
     LowerCmdArgs.push_back("216");
     LowerCmdArgs.push_back("1");
-    // Lower: -ieee 1
+    // Common: -ieee 1
     CommonCmdArgs.push_back("-ieee");
     CommonCmdArgs.push_back("1");
   } else if (NeedRelaxedMath) {
@@ -634,11 +634,11 @@ void ClassicFlang::ConstructJob(Compilation &C, const JobAction &JA,
     LowerCmdArgs.push_back("-y");
     LowerCmdArgs.push_back("216");
     LowerCmdArgs.push_back("1");
-    // Lower: -ieee 0
+    // Common: -ieee 0
     CommonCmdArgs.push_back("-ieee");
     CommonCmdArgs.push_back("0");
   } else {
-    // Lower: -ieee 0
+    // Common: -ieee 0
     CommonCmdArgs.push_back("-ieee");
     CommonCmdArgs.push_back("0");
   }
