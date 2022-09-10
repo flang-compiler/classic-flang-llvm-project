ac17ff4d2472e90e35615f711f1737d1345d5900
Bryan Chan <bryan.chan@huawei.com>
2022-09-10 19:41:06 -0400

    [Driver] Pass Classic Flang libraries to the linker correctly
    In LLVM 15, the Fortran and OpenMP runtime libraries are added to the linker
    command line using common methods (addFortranRuntime* and addOpenMPRuntime*).
    This commit adds Classic Flang awareness to addFortranRuntime*, so that
    Classic Flang doesn't attempt to link with LLVM Flang libraries. Re-using
    the same methods as Clang and LLVM Flang also helps reduce downstream delta.
    A Classic Flang test is added to ensure that the linker command is constructed
    correctly.

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -947,7 +947,11 @@ bool tools::addOpenMPRuntime(ArgStringList &CmdArgs, const ToolChain &TC,
                              const ArgList &Args, bool ForceStaticHostRuntime,
                              bool IsOffloadingHost, bool GompNeedsRT) {
   if (!Args.hasFlag(options::OPT_fopenmp, options::OPT_fopenmp_EQ,
-                    options::OPT_fno_openmp, false))
+                    options::OPT_fno_openmp, false)
+#ifdef ENABLE_CLASSIC_FLANG
+      && !Args.hasFlag(options::OPT_mp, options::OPT_nomp, false)
+#endif
+     )
     return false;
 
   Driver::OpenMPRuntimeKind RTKind = TC.getDriver().getOpenMPRuntime(Args);
@@ -992,7 +996,16 @@ bool tools::addOpenMPRuntime(ArgStringList &CmdArgs, const ToolChain &TC,
 }
 
 void tools::addFortranRuntimeLibs(const ToolChain &TC,
+#ifdef ENABLE_CLASSIC_FLANG
+                                  const llvm::opt::ArgList &Args,
+#endif
                                   llvm::opt::ArgStringList &CmdArgs) {
+#ifdef ENABLE_CLASSIC_FLANG
+  if (needFortranLibs(TC.getDriver(), Args))
+    TC.AddFortranStdlibLibArgs(Args, CmdArgs);
+  else
+    Args.ClaimAllArgs(options::OPT_noFlangLibs);
+#else
   if (TC.getTriple().isKnownWindowsMSVCEnvironment()) {
     CmdArgs.push_back("Fortran_main.lib");
     CmdArgs.push_back("FortranRuntime.lib");
@@ -1002,6 +1015,7 @@ void tools::addFortranRuntimeLibs(const ToolChain &TC,
     CmdArgs.push_back("-lFortranRuntime");
     CmdArgs.push_back("-lFortranDecimal");
   }
+#endif
 }
 
 void tools::addFortranRuntimeLibraryPath(const ToolChain &TC,
