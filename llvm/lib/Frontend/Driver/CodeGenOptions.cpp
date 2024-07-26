cc1a33a424c2f199a14fb816605e54f57b4d1941
Shivarama Rao <shivarama.rao@amd.com>
2024-07-26 05:11:33 +0000

    Fix for Issue #1413
    PGMATH has AVX512 runtime functions and can be executed only when the application is compiled
    in avx512 mode. The VecFuncs.def has no information about the TargetOptions and avx512 functions
    are selected even in avx2 mode. This issue is fixed by creating separate table for AVX512 functions
    and using them only when avx512 mode is specified.

--- llvm/lib/Frontend/Driver/CodeGenOptions.cpp
+++ llvm/lib/Frontend/Driver/CodeGenOptions.cpp
@@ -13,7 +13,8 @@
 namespace llvm::driver {
 
 TargetLibraryInfoImpl *createTLII(llvm::Triple &TargetTriple,
-                                  driver::VectorLibrary Veclib) {
+                                  driver::VectorLibrary Veclib,
+                                  bool TargetHasAVX512) {
   TargetLibraryInfoImpl *TLII = new TargetLibraryInfoImpl(TargetTriple);
 
   using VectorLibrary = llvm::driver::VectorLibrary;
@@ -34,7 +35,11 @@ TargetLibraryInfoImpl *createTLII(llvm::Triple &TargetTriple,
   case VectorLibrary::PGMATH:
     TLII->addVectorizableFunctionsFromVecLib(TargetLibraryInfoImpl::PGMATH,
                                              TargetTriple);
+    if (TargetHasAVX512)
+      TLII->addVectorizableFunctionsFromVecLib(
+          TargetLibraryInfoImpl::PGMATH_AVX512, TargetTriple);
     break;
+
 #endif
   case VectorLibrary::SVML:
     TLII->addVectorizableFunctionsFromVecLib(TargetLibraryInfoImpl::SVML,
